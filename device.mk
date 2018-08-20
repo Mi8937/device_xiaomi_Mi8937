2bcacf10c011abb04972592c1f22a236b4e69749
Nich <nctrenco@gmail.com>
2018-08-20 21:40:21 +0800

    Mi8937: land: Add pthread_mutex_destroy hack for camera
    In Pie, Google added checks to prevent destroying of the same mutexes
    consecutively, and aborts the program on fortify fatal call.
    
    Our camera blobs trigger this check when tearing down an ISP session,
    destroying the same mutex consecutively by adding a destroy instruction
    in a loop. This causes the whole daemon to crash and fail.
    
    (Specifically, the violating instructions were from isp_module_stop_session
    and when isp_module_start_session fails to create a thread.)
    
    Workaround the issue by shimming the function; catching destroyed
    mutexes and returning -EBUSY without passing on the bad mutexes to libc.
    
    [cafebabe: Load it using patchelf --add-needed instead]
    
    Change-Id: I4f8542a496dc25c53f8208a647cc0d6acc8662eb

--- device.mk
+++ device.mk
@@ -85,6 +85,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libbinder_shim \
     libfakelogprint \
+    libshim_mutexdestroy \
     libshim_pthreadts \
     libshims_android \
     libshims_ui \
