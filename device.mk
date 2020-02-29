391bd0a5e49de453e2d9ee57ae4e0916abd5742c
Daniel Rosenberg <drosen@google.com>
2020-02-28 17:55:47 -0800

    Mi8937: Enable casefolding support on userdata
    This enables casefolding, which is required for removal of sdcardfs.
    
    Test: Wipe data and boot. lsattr /data/media, check for +F
    Bug: 138322712
    Change-Id: I020aa6bb9c121e7bda83c07bc9fb37dcfa26702c

--- device.mk
+++ device.mk
@@ -47,10 +47,8 @@ PRODUCT_PACKAGES += \
     camera.ulysse \
     camera.wingtech
 
-# Enable wider inodes for project quotas
-# Later, replace with $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-PRODUCT_QUOTA_PROJID := 1
-PRODUCT_PRODUCT_PROPERTIES += ro.emulated_storage.projid=1
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Fingerprint
 PRODUCT_COPY_FILES += \
