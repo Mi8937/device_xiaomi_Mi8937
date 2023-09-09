74f94350b314eb620eb12132be14b1ad106d98bc
me-cafebabe <me.cafebabe@gmail.com>
2023-09-09 18:53:53 +0800

    Mi8937: Remove libshim_mutexdestroy and libshim_pthreadts
    * Prepare to reimport them with full commit chain
    
    Change-Id: Ibb48bb9de055a9f043abbb9eb3b2338e2d76e9f5

--- device.mk
+++ device.mk
@@ -85,8 +85,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libbinder_shim \
     libfakelogprint \
-    libshim_mutexdestroy \
-    libshim_pthreadts \
     libshims_android \
     libshims_ui \
     libwui
