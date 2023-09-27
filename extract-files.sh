8ab38b40ed13bf2c85286cb1feceec52e6fae5b5
Alexander Martinz <amartinz@shiftphones.com>
2023-09-27 17:13:12 +0200

    Mi8937: fingerprint: use libhidlbase-v32 for ugg goodix
    [cafebabe: Adapt to our usecase, Switch to patchelf v0.17.2]
    
    Test: run cts-on-gsi -m CtsBiometricsTestCases
    Test: run cts-on-gsi -m CtsHardwareTestCases -t android.hardware.fingerprint.cts.FingerprintManagerTest
    Test: run vts -m VtsHalBiometricsFingerprintV2_1TargetTest
    Test: run vts -m VtsHalBiometricsFingerprintV2_2TargetTest
    Change-Id: Iaef8ede57250aa171f9863e7e250c604ef241334

--- extract-files.sh
+++ extract-files.sh
@@ -71,6 +71,9 @@ function blob_fixup() {
 	vendor/lib64/libgf_ca.so)
             sed -i 's|/system/etc/firmware|////vendor/firmware/ugg|g' "${2}"
             ;;
+	vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so)
+            "${PATCHELF_0_17_2}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
+            ;;
         vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so)
             "${PATCHELF_0_17_2}" --remove-needed "libprotobuf-cpp-lite.so" "${2}"
             ;;
