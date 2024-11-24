c281b84c7830c29f38beecbc66da2e6204bcb440
Yumi Yukimura <me.cafebabe@gmail.com>
2024-11-24 17:18:57 +0800

    Mi8937: Add product makefile for Lindroid builds
    Change-Id: Ic15b311db3912cc52b98688025f45926a8085eaa

--- device.mk
+++ device.mk
@@ -52,12 +52,14 @@ PRODUCT_PACKAGES += \
     libdumpstate_device
 
 ifeq ($(TARGET_KERNEL_VERSION),4.19)
+ifneq ($(MITHORIUM_INTEGRATE_LINDROID),true)
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Use FUSE passthrough
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.fuse.passthrough.enable=true
+endif
 endif
 
 # Fingerprint
