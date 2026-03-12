LineageOS/android_device_xiaomi_Mi8937

Mi8937: libstdc++.vendor => libstdc++_vendor
Change-Id: I478eb89aa2ac106d855b168844863032612bc8e8

extract-files.sh
@@ -71,6 +71,11 @@ function blob_fixup() {
             "${PATCHELF_0_8}" --remove-needed "libprotobuf-cpp-lite.so" "${2}"
             ;;
     esac
+
+    # For all ELF files
+    if [[ "${1}" =~ ^.*(\.so|\/bin\/.*)$ ]]; then
+        "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+    fi
 }
 
 # If we're being sourced by the common script that we called,

Mi8937/device_xiaomi_Mi8937

extract-files.sh
@@ -76,6 +76,11 @@ function blob_fixup() {
             "${PATCHELF_0_17_2}" --remove-needed "libprotobuf-cpp-lite.so" "${2}"
             ;;
     esac
+
+    # For all ELF files
+    if [[ "${1}" =~ ^.*(\.so|\/bin\/.*)$ ]]; then
+        "${PATCHELF_0_17_2}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+    fi
 }
 
 # If we're being sourced by the common script that we called,