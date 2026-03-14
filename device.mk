Mi-Thorium/android_device_xiaomi_Mi8937

Mi8937: audio: Decommonize platform info xml for land and santoni
* land and santoni has stock acdb table extracted by other people, import it
* for the other devices, I'll do it later
* Also, Refactor audio/ directory while we're at it

Change-Id: I1d0b488c0dbbdd1ddfea50dea9edef6fb473c3cc

device.mk
@@ -38,8 +38,8 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*mixer_paths_*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_ODM)/etc/) \
-    $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml
+    $(call find-copy-subdir-files,*.xml,$(LOCAL_PATH)/audio/mixer_paths/,$(TARGET_COPY_OUT_ODM)/etc/) \
+    $(call find-copy-subdir-files,*.xml,$(LOCAL_PATH)/audio/platform_info/,$(TARGET_COPY_OUT_ODM)/etc/)
 
 # Camera
 PRODUCT_COPY_FILES += \

Mi8937/device_xiaomi_Mi8937

device.mk
@@ -33,7 +33,8 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc/)
+    $(call find-copy-subdir-files,*.xml,$(LOCAL_PATH)/audio/mixer_paths,$(TARGET_COPY_OUT_VENDOR)/etc/) \
+    $(call find-copy-subdir-files,*.xml,$(LOCAL_PATH)/audio/platform_info/,$(TARGET_COPY_OUT_VENDOR)/etc/)
 
 # Camera
 PRODUCT_COPY_FILES += \